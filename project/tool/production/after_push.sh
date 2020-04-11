e28bb9b7e079cc2d97442bb90e2276120e038fb5
yaoyang <kiki@smarty.so>
2020-04-11 22:13:55 +0800

    Fork from git@github.com:smarty-kiki/mvc_frame.git and modify project name

--- project/tool/production/after_push.sh
+++ project/tool/production/after_push.sh
@@ -2,15 +2,15 @@
 
 ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
 
-ln -fs $ROOT_DIR/project/config/production/nginx/mvc_frame.conf /etc/nginx/sites-enabled/mvc_frame
+ln -fs $ROOT_DIR/project/config/production/nginx/map.conf /etc/nginx/sites-enabled/map
 /usr/sbin/service nginx reload
 
 /bin/bash $ROOT_DIR/project/tool/dep_build.sh link
 /usr/bin/php $ROOT_DIR/public/cli.php migrate:install
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
-ln -fs $ROOT_DIR/project/config/production/supervisor/mvc_frame_queue_worker.conf /etc/supervisor/conf.d/mvc_frame_queue_worker.conf
+ln -fs $ROOT_DIR/project/config/production/supervisor/map_queue_worker.conf /etc/supervisor/conf.d/map_queue_worker.conf
 /usr/bin/supervisorctl update
-/usr/bin/supervisorctl restart mvc_frame_queue_worker:*
+/usr/bin/supervisorctl restart map_queue_worker:*
 
-ln -fs $ROOT_DIR/project/config/production/crontab/mvc_frame /etc/cron.d/mvc_frame
+ln -fs $ROOT_DIR/project/config/production/crontab/map /etc/cron.d/map
