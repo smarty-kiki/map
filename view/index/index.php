855fc0d62c52a911467f246529773aeb1075c0a9
yaoyang <kiki@smarty.so>
2020-04-12 15:05:57 +0800

    debug 坐标转换偏移问题

--- view/index/index.php
+++ view/index/index.php
@@ -15,10 +15,13 @@
     <div id="container"></div>
 <script>
 navigator.geolocation.getCurrentPosition(function (position) {
+
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
+
     var map = new BMapGL.Map("container");          // 创建地图实例
-    var point = new BMapGL.Point(latitude, longitude);  // 创建点坐标
+    var point = new BMapGL.Point(longitude, latitude);  // 创建点坐标
+
     map.centerAndZoom(point, 15);                 // 初始化地图，设置中心点坐标和地图级别
     map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
 });
