af87a630a4ca211f68e038fe6d315c5e6e3967ae
yaoyang <kiki@smarty.so>
2020-04-12 15:17:58 +0800

    debug https 加载 http js 的问题

--- view/index/index.php
+++ view/index/index.php
@@ -4,7 +4,8 @@
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>地图</title>
-    <script type="text/javascript" src="https://api.map.baidu.com/api?v=1.0&type=webgl&ak=jWMAZ7jMYfp8r6QDV7q7iZx9lbrIuGc3"></script>
+    <script type="text/javascript" src="//api.map.baidu.com/api?v=1.0&type=webgl&ak=jWMAZ7jMYfp8r6QDV7q7iZx9lbrIuGc3"></script>
+    <script type="text/javascript" src="//api.map.baidu.com/api?v=2.0&ak=jWMAZ7jMYfp8r6QDV7q7iZx9lbrIuGc3"></script>
     <style type="text/css">
         html{height:100%}
         body{height:100%;margin:0px;padding:0px}
@@ -22,7 +23,7 @@ navigator.geolocation.getCurrentPosition(function (position) {
     var map = new BMapGL.Map("container");          // 创建地图实例
     var point = new BMapGL.Point(gps_longitude, gps_latitude);  // 创建点坐标
 
-    var convertor = new BMapGL.Convertor();
+    var convertor = new BMap.Convertor();
     var pointArr = [];
     pointArr.push(point);
     convertor.translate(pointArr, 1, 5, function (data) {
@@ -30,8 +31,8 @@ navigator.geolocation.getCurrentPosition(function (position) {
 
             var bd_point = data.points[0];
 
-            var marker = new BMapGL.Marker(bd_point);
-            var label = new BMapGL.Label("当前在这里",{offset:new BMapGL.Size(20,-10)});
+            var marker = new BMap.Marker(bd_point);
+            var label = new BMap.Label("当前在这里",{offset:new BMap.Size(20,-10)});
             marker.setLabel(label); //添加百度label
 
             map.addOverlay(marker);
